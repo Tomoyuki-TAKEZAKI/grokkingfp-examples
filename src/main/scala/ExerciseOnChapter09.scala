196bb2bb70ee2424a8e76e21e0793a66dfe4edcf
t-takezaki <49390028+Tomoyuki-TAKEZAKI@users.noreply.github.com>
2024-04-15 23:37:37 +0900

    exercise 9.40

--- src/main/scala/ExerciseOnChapter09.scala
+++ src/main/scala/ExerciseOnChapter09.scala
@@ -1,6 +1,8 @@
 import cats.effect.IO
 import ch09_CurrencyExchange.exchangeRatesTableApiCall
 import ch08_SchedulingMeetings.retry
+import fs2.Stream
+import ch08_CastingDie.castTheDieImpure
 
 object ExerciseOnChapter09 {
 
@@ -75,4 +77,33 @@ object Exercise0906 {
   val valueFromM3: Option[String] = m3.get("key")
 
   val valueFromM4: Option[String] = m4.get("key")
+}
+
+object Exercise0940 {
+  def castTheDie(): IO[Int] = IO.delay(castTheDieImpure())
+
+  val infiniteDieCasts: Stream[IO, Int] = Stream.eval(castTheDie()).repeat
+
+  val s1: IO[List[Int]] = infiniteDieCasts.filter(_ % 2 != 0).take(3).compile.toList
+
+  val s2: IO[List[Int]] = infiniteDieCasts.take(5).map(n => if (n == 6) n * 2 else n).compile.toList
+
+  val s3: IO[List[Int]] = infiniteDieCasts.take(3).fold(0)(_ + _).compile.toList // fold せずに結果をマップしてたしても良い
+
+  val s3_ : IO[List[Int]] = infiniteDieCasts.take(3).scan(0)((acc, n) => acc + n).compile.toList.map(_.drop(3)) // scan 使ってみたがスマートなやり方がありそうだ
+
+  val s4: IO[List[Int]] = infiniteDieCasts.filter(_ == 5).take(1).append(infiniteDieCasts.take(2)).compile.toList
+
+  // 問題4で、最後の3つだけではなく全ての目を返す場合
+  val s4_ : IO[List[Int]] = infiniteDieCasts.takeThrough(_ != 5).append(infiniteDieCasts.take(2)).compile.toList
+
+  val s5: IO[Unit] = infiniteDieCasts.take(100).compile.drain
+
+  val s6: IO[List[Int]] = infiniteDieCasts.take(3).append(infiniteDieCasts.take(3).map(_ * 3)).compile.toList
+
+  val s7: IO[List[(Int, Int)]] = infiniteDieCasts
+    .scan((0, 0))((acc, current) => (acc._2, current))
+    .filter((prev, current) => prev == 6 && current == 6)
+    .take(1).compile.toList
+  // 解答例のように、 acc の値を current が 6 なら acc + 1, 6 以外なら 0 にする実装でも良い。
 }
