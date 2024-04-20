254f8110a89b0692c3b9383268c250eaaaf6f190
t-takezaki <49390028+Tomoyuki-TAKEZAKI@users.noreply.github.com>
2024-04-20 22:12:47 +0900

    exercise 10.4 first attempt

--- src/main/scala/ExerciseOnChapter10.scala
+++ src/main/scala/ExerciseOnChapter10.scala
@@ -4,13 +4,13 @@ import fs2.Stream
 object ExerciseOnChapter10 {
   opaque type City = String
 
-  object City {
+  private object City {
     def apply(name: String): City = name
 
     extension (city: City) def name: String = city
   }
 
-  case class CityStats(city: City, checkIns: Int)
+  private case class CityStats(city: City, checkIns: Int)
 
   val checkIns: Stream[IO, City] =
     Stream(
@@ -21,7 +21,28 @@ object ExerciseOnChapter10 {
       City("Cape Town"),
       City("Sydney"),
     ).covary[IO]
-    
-  def processCheckIn(checkIns: Stream[IO, City]) : IO[Unit] = ???
 
+  def processCheckIn(checkIns: Stream[IO, City]): IO[Unit] =
+    scanCheckIn(checkIns)
+      .map(convertToCityStats)
+      .map(sortCityStats)
+      .take(3)
+      .foreach(cityStats => IO.delay(println(cityStats)))
+      .compile
+      .drain
+
+  private def scanCheckIn(checkIns: Stream[IO, City]): Stream[IO, Map[City, Int]] =
+    checkIns
+      .scan(Map.empty[City, Int])((map, city) =>
+        map.updatedWith(city) {
+          case Some(count) => Some(count + 1)
+          case None => Some(1)
+        }
+      )
+
+  private def convertToCityStats(map: Map[City, Int]): List[CityStats] =
+    map.toList.map((city, count) => CityStats(city, count))
+
+  private def sortCityStats(list: List[CityStats]): List[CityStats] =
+    list.sortBy(_.checkIns).reverse
 }
