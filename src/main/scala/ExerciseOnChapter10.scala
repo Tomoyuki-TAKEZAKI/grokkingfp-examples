b35bf7680ea8f8e5b19bc6fd89127a4e54bd79e6
t-takezaki <49390028+Tomoyuki-TAKEZAKI@users.noreply.github.com>
2024-04-21 22:13:08 +0900

    exercise 10.17

--- src/main/scala/ExerciseOnChapter10.scala
+++ src/main/scala/ExerciseOnChapter10.scala
@@ -1,5 +1,10 @@
-import cats.effect.IO
+import cats.effect.{IO, Ref}
 import fs2.Stream
+import cats.implicits.*
+import ch10_CastingDieConcurrently.castTheDie
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
 
 object ExerciseOnChapter10 {
   opaque type City = String
@@ -54,4 +59,32 @@ object ExerciseOnChapter10 {
       .sortBy(_.checkIns)
       .reverse
       .take(3)
+}
+
+object Exercise1017 {
+  val p1: IO[Int] = for {
+    _ <- IO.sleep(FiniteDuration(1, TimeUnit.SECONDS))
+    result <- List.fill(2)(castTheDie()).sequence.map(_.sum)
+  } yield result
+
+  val p2: IO[Ref[IO, List[Int]]] = for {
+    result <- List.fill(2)(castTheDie()).parSequence
+    ref <- Ref.of[IO, List[Int]](result)
+  } yield ref
+
+  val p3: IO[Ref[IO, List[Int]]] = for {
+    result <- List.fill(3)(castTheDie()).parSequence
+    ref <- Ref.of[IO, List[Int]](result)
+  } yield ref
+
+  val p4: IO[Ref[IO, Int]] = for {
+    result <- List.fill(100)(castTheDie()).parSequence.map(_.count(_ == 6))
+    ref <- Ref.of[IO, Int](result)
+  } yield ref
+
+  val p5: IO[Int] = for {
+    result <- List.fill(100)(
+      IO.sleep(FiniteDuration(1, TimeUnit.SECONDS)).flatMap(_ => castTheDie())
+    ).parSequence
+  } yield result.sum
 }
